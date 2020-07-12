07c1b2ee1bd16eb244d1b0d026417da8683c3d2f
Emma JGo <emmacjg@gmail.com>
2020-07-11 23:28:18 -0400

    adds color without resolution

--- script.js
+++ script.js
@@ -8,40 +8,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const submit = document.getElementById('submit');
     const palette = document.getElementById('palette');
     const grid = document.getElementById('grid');
+    const dots = document.getElementsByClassName('dot');
 
     // Basic indexed RGB values for each color for the 5x5 grid (g5)
     let basicRgbObjects = {
         black: {
             g3: {}, g10: {},
             g5: {
-                    i0:  [[41,41,41]],
-                    // i5:  [[54,54,54]], ORIGINAL
-                    i5:  [[64,64,64]],
-                    i10: [[189,189,189]],
-                    // i15: [[221,221,221]], ORIGINAL
-                    i15: [[214,214,214]],
-                    // i20: [[238,238,238]]  ORIGINAL
-                    i20: [[235,235,235]]
+                    i0: [[21,21,21]],
+                    i5: [[100,100,100]],
+                    i10: [[155,155,155]],
+                    i15: [[189,189,189]],
+                    i20: [[214,214,214]]
                 }
         },
         brown: {
             g3: {}, g10 : {},
             g5: {
-                i0:  [[84,50,48]], 
-                i5:  [[103,66,53]],
-                i10: [[124,84,56]],
-                i15: [[148,103,58]],
-                i20: [[169,125,57]]
+                // i0:  [[82,50,48]], 
+                // i5:  [[103,66,53]],
+                // i10: [[124,84,56]],
+                // i15: [[148,103,58]],
+                // i20: [[169,125,57]]
+
+                i0:  [[82,50,48]], 
+                i5:  [[113,66,53]],
+                i10: [[144,84,56]],
+                i15: [[175,103,58]],
+                i20: [[206,125,57]]
             }
         },
         blue: {
             g3: {}, g10: {},
             g5: {
-                i0:  [[0,85,255]],
-                i5:  [[0,146,255]],
-                i10: [[148,197,255]],
-                i15: [[26,50,175]],
-                i20: [[6,16,39]]
+                i0: [[6,16,39]],
+                i5: [[26,50,175]],
+                i10:  [[0,85,255]],
+                i15:  [[0,146,255]],
+                i20: [[148,197,255]]
             }
         },
         red: {
@@ -64,6 +68,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 i20: [[255,255,231]]
             }
         }
+    }
+
+    //Triad indexed value to change each rgb 
+    const indexedChanges = {
+        black: [1,1,1],
+        brown: [1,0,0], 
+        blue: [0,1,0],
+        red: [0,1,1],
+        yellow: [0,0,1]
     }
 
     /**
@@ -107,7 +120,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const color = triadResult.color;
         if (gNumber == null || gResolution == null || color == null) 
         {
-        alert('You have to make selections for the three fields! Try again! ');
+            alert('You have to make selections for the three fields! Try again! ');
+            gNumber = gResolution = color = null;
         }
         else {
             submit.style.backgroundColor = 'lightgray';
@@ -123,11 +137,9 @@ document.addEventListener('DOMContentLoaded', () => {
      * @param {number} gNumber 
      */
     function generateGrid(gNumber) {
-        let count = 0;
         for ( let i = 0; i < gNumber; i ++) {
             for ( let j = 0; j < gNumber; j ++) {
-                grid.innerHTML+= `<div class='dot' id='dot${count}'>${count}</div>`;
-                count ++;
+                grid.innerHTML+= `<div class='dot'></div>`;
             }
             grid.innerHTML += '<br>';
         }
@@ -149,21 +161,48 @@ document.addEventListener('DOMContentLoaded', () => {
         return (rgb);
     }
     
+    //NOT SURE IF NEEDED FOR OBJECTOFRGBS
+    // /**
+    //  * Generates rgb text for a whole object with array of arrays (i.e. g5)
+    //  *  of triads of values
+    //  * @param {object of object of arrays} objectOfRgbs 
+    //  */
+    // function rgbTextArrayGenerator(objectOfRgbs) {
+    //     const rgbTextArray = [];
+    //     for ( let key in objectOfRgbs ) {
+    //         for ( let i = 0; i < objectOfRgbs[key].length; i ++) {
+    //             rgbTextArray.push(rgbTextGenerator(objectOfRgbs[key][i]));
+    //         }
+    //     }
+    //     return (rgbTextArray);
+    // }
+
+    
     /**
-     * Generates rgb text for a whole object with array of arrays (i.e. g5)
-     *  of triads of values
-     * @param {object of object of arrays} objectOfRgbs 
+     * Generages initial values for g3 object for a given color
+     * @param {object} colorObject 
      */
-    function rgbTextArrayGenerator(objectOfRgbs) {
-        const rgbTextArray = [];
-        for ( let key in objectOfRgbs ) {
-            for ( let i = 0; i < objectOfRgbs[key].length; i ++) {
-                rgbTextArray.push(rgbTextGenerator(objectOfRgbs[key][i]));
-            }
-        }
-        return (rgbTextArray);
+    function generatesBaseG3(color) {
+        basicRgbObjects[color].g3.i0 = basicRgbObjects[color].g5.i0;
+        basicRgbObjects[color].g3.i3 = basicRgbObjects[color].g5.i10;
+        basicRgbObjects[color].g3.i6 = basicRgbObjects[color].g5.i20;
+        return (basicRgbObjects[color].g3);
+    }
+    
+    /**
+     * Generates initial values for g10 object for a given color
+     * @param {object} colorObject 
+     */
+    function generatesBaseG10(color) {
+        basicRgbObjects[color].g10.i0 = basicRgbObjects[color].g5.i0;
+        basicRgbObjects[color].g10.i20 = basicRgbObjects[color].g5.i5;
+        basicRgbObjects[color].g10.i40 = basicRgbObjects[color].g5.i10;
+        basicRgbObjects[color].g10.i60 = basicRgbObjects[color].g5.i15;
+        basicRgbObjects[color].g10.i80 = basicRgbObjects[color].g5.i20;
+        return (basicRgbObjects[color].g10);
     }
 
+    // WORKING ON IT --- > GRESOLUTION
     /**
      * Determines spacing based on grid resolution
      * @param {number} gResolution 
@@ -177,89 +216,86 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         return (spacing);
     }
-    
-    /**
-     * Generages initial values for g3 object for a given color
-     * @param {object} colorObject 
-     */
-    function generatesBaseG3(colorObject) {
-        colorObject.g3.i0 = colorObject.g5.i0;
-        colorObject.g3.i3 = colorObject.g5.i10;
-        colorObject.g3.i6 = colorObject.g5.i20;
-        return (colorObject.g3);
-    }
-    
-    /**
-     * Generates initial values for g10 object for a given color
-     * @param {object} colorObject 
-     */
-    function generatesBaseG10(colorObject) {
-        colorObject.g10.i0 = colorObject.g5.i0;
-        colorObject.g10.i20 = colorObject.g5.i5;
-        colorObject.g10.i40 = colorObject.g5.i10;
-        colorObject.g10.i60 = colorObject.g5.i15;
-        colorObject.g10.i80 = colorObject.g5.i20;
-        return (colorObject.g10);
-    }
 
-     /**
-     * Generates new array for black by adding 1 to each in triad
+    // WORKING ON IT --- > GRESOLUTION
+    /**
+     * Specifically alters individuals rgb triads according to color
      * @param {array} singleRgbArray 
+     * @param {array} indexedChanges 
      */
-    function alteredBlackRgbArrayGen(singleRgbArray) {
+    function specificAlteredRgb(singleRgbArray,color) {
         let newN = 0;
         let newRgbArray = [];
-        let spacing = 1;
-        
         for ( let i = 0; i < 3; i ++) {
-            newN = singleRgbArray[i] + spacing;
+            newN = singleRgbArray[i] + indexedChanges[color][i];
             newRgbArray.push(newN);
             newN = 0;
         }
         return (newRgbArray);
     }
 
-    // NEW WORKING ON
-    function specificAlteredRgb(singleRbbArray,indexTriad,changesTriad) {
-
-    }
-
     /**
-     * Generates full basic rgb for 3x3, 5x5, or 10x10 black grid
-     * @param {object} colorObject 
+     * Generates full basic values for 3x3, 5x5, or 10x10 grid
+     * @param {string} color 
      * @param {number} gNumber 
      */
-    function generatesFullBlackG (colorObject,gNumber) {
+    function generatesFullColorG(color,gNumber) {
         let gArray;
         let limit_1 = gNumber;
+        let limit_2 = gNumber;
         if ( gNumber == 3) {
-            generatesBaseG3(colorObject);
-            gArray = [colorObject.g3.i0, colorObject.g3.i3, colorObject.g3.i6];
+            generatesBaseG3(color);
+            gArray = [basicRgbObjects[color].g3.i0, basicRgbObjects[color].g3.i3, basicRgbObjects[color].g3.i6];
         } else if ( gNumber == 10 ) {
             limit_1 = gNumber / 2;
             limit_2 = gNumber * 2;
-            generatesBaseG10(colorObject);
-            gArray = [colorObject.g10.i0, colorObject.g10.i20, colorObject.g10.i40, colorObject.g10.i60, colorObject.g10.i80];
+            generatesBaseG10(color);
+            gArray = [basicRgbObjects[color].g10.i0, basicRgbObjects[color].g10.i20, basicRgbObjects[color].g10.i40, basicRgbObjects[color].g10.i60, basicRgbObjects[color].g10.i80];
         } else {
             gNumber == 5;
-            gArray = [colorObject.g5.i0, colorObject.g5.i5, colorObject.g5.i10, colorObject.g5.i15, colorObject.g5.i20];
+            gArray = [basicRgbObjects[color].g5.i0, basicRgbObjects[color].g5.i5, basicRgbObjects[color].g5.i10, basicRgbObjects[color].g5.i15, basicRgbObjects[color].g5.i20];
         }
         
         for (  let j = 0; j < limit_1; j ++) {
             for ( let i = 1; i < limit_2; i ++) { 
-                gArray[j][i] = alteredBlackRgbArrayGen(gArray[j][i-1]);
+                gArray[j][i] = specificAlteredRgb(gArray[j][i-1],color);
             }
         } 
-
-        if ( gNumber == 3) {
-            return (colorObject.g3);
-        } else if ( gNumber == 10 ) {
-            return (colorObject.g10);
-        } else {
-            return (colorObject.g5);
-        }
+        return(gArray);
+        // if ( gNumber == 3) {
+        //     return(gArray);
+        //     // return (basicRgbObjects[color].g3);
+        // } else if ( gNumber == 10 ) {
+        //     return (basicRgbObjects[color].g10);
+        // } else {
+        //     return (basicRgbObjects[color].g5);
+        // }
     }
 
+    /**
+     * Gives rgb value to each circle in grid from values in array
+     * @param {number} gNumber 
+     * @param {array} colorArray 
+     */
+    function colorTheDots(gNumber,colorArray) {
+        if ( gNumber == 10 ) {
+            limit1 = gNumber / 2;
+            limit2 = gNumber * 2;
+        } else {
+            limit1 = gNumber;
+            limit2 = gNumber;
+        }
+        let count = 0;
+        for ( let i = 0; i < limit1; i ++) {
+            for (let j = 0; j < limit2; j ++) {
+                let color = rgbTextGenerator(colorArray[i][j]);
+                dots[count].style.backgroundColor = color;
+                dots[count].textContent = color;
+                count ++;
+            }   
+        }
+    }
+    
     submit.addEventListener('click', () => {
         // Initializes variables with user's info selected from form
         const triadResult = {};
@@ -272,15 +308,13 @@ document.addEventListener('DOMContentLoaded', () => {
         
         // Validates that three fields of form are filled
         validateInfo(triadResult);   
-        //Generates grid  
+        //Generates grid of circles
         generateGrid(gNumber);
-
-        //Generates rgb for whole OBJECT of arrays
-        // rgbTextArrayGenerator(basicRgbObjects.blue.g5)
-
-
-        // BLACK READY
-        // console.log(generatesFullBlackG(basicRgbObjects.black,gNumber));
+        //Generates color values for given grid
+        const colorArray = generatesFullColorG(color,gNumber);
+        console.log(colorArray);
+        //Colors the circles in grid
+        colorTheDots(gNumber,colorArray);
         
     });
 });
